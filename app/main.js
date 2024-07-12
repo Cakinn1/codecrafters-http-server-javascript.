9e9dee5aed3ead35997b0e4aaf5b8fc1de0be078
anthony <anthonycakins@gmail.com>
2024-07-12 16:22:02 +1000

    testing

--- app/main.js
+++ app/main.js
@@ -4,15 +4,23 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-
   socket.on("data", (data) => {
-    const requestString = data.toString()
-    console.log(requestString, "data");
-  });
+    const requestString = data.toString();
 
-  socket.on("close", () => {
-    socket.end();
+    const requestTarget = requestString.split(" ")[1];
+    console.log("request:", requestTarget, requestString);
+
+    if (requestTarget === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      socket.on("close", () => {
+        socket.end();
+      });
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      socket.on("close", () => {
+        socket.end();
+      });
+    }
   });
 });
 
