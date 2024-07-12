b20c22bf1fc895a86f78dbe1cedd29e160abb5fd
anthony <anthonycakins@gmail.com>
2024-07-12 16:02:49 +1000

    test

--- app/main.js
+++ app/main.js
@@ -6,9 +6,14 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.write("HTTP/1.1 200 OK\r\n\r\n");
 
+  socket.on('data', (d) => {
+console.log(d, 'data')
+  })
+
+
   socket.write("GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n")
 
-
+    socket.write("GET / HTTP/1.1")
 
   socket.on("close", () => {
     socket.end();
