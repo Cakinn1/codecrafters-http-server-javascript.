5b43886d73f7fdb945007f7ef9d0fc84f5cc93c8
anthony <anthonycakins@gmail.com>
2024-07-13 11:26:54 +1000

    t

--- app/main.js
+++ app/main.js
@@ -6,21 +6,27 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const requestString = data.toString();
-
     const requestTarget = requestString.split(" ")[1];
-    console.log("request:", requestTarget, requestString);
 
-    if (requestTarget === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-      socket.on("close", () => {
-        socket.end();
-      });
+    const okHTTPResponse = "HTTP/1.1 200 OK\r\n\r\n";
+    const notFoundHTTPResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+    console.log(requestTarget.split("/"), requestString);
+
+    const echoResponse = requestTarget.split("/")[1];
+    const echoStrResponse = requestTarget.split("/")[2]
+
+    if (echoResponse === "echo") {
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoStrResponse.length}\r\n\r\n${echoResponse}`
+      );
+    } else if (requestTarget === "/") {
+      socket.write(okHTTPResponse);
+      console.log("ok");
     } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-      socket.on("close", () => {
-        socket.end();
-      });
+      socket.write(notFoundHTTPResponse);
+      console.log("404");
     }
+    socket.end();
   });
 });
 
