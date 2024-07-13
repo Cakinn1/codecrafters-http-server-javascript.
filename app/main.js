bc278fb12ae7fc4cc16358494d2317586a975bc2
anthony <anthonycakins@gmail.com>
2024-07-13 11:57:59 +1000

    t

--- app/main.js
+++ app/main.js
@@ -20,6 +20,7 @@ const server = net.createServer((socket) => {
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
       );
+    //   test
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
