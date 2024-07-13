85f64acf4805d26d818b636051491fdd020fadef
anthony <anthonycakins@gmail.com>
2024-07-13 11:52:50 +1000

    t

--- app/main.js
+++ app/main.js
@@ -6,25 +6,22 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const requestString = data.toString();
-    const requestTarget = requestString.split(" ")[1];
+    const url = requestString.split(" ")[1];
 
-    const okHTTPResponse = "HTTP/1.1 200 OK\r\n\r\n";
-    const notFoundHTTPResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
-    console.log(requestTarget.split("/"), requestString);
-
-    const echoResponse = requestTarget.split("/")[1];
-    const echoStrResponse = requestTarget.split("/")[2]
-
-    if (echoResponse === "echo") {
+    if (url === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (url.startsWith("/echo/")) {
+      const str = url.split("/echo/")[1];
       socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoStrResponse.length}\r\n\r\n${echoStrResponse}`
+        `HTTP/1.1 200 ok\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`
       );
-    } else if (requestTarget === "/") {
-      socket.write(okHTTPResponse);
-      console.log("ok");
+    } else if (url.startsWith("/user-agent")) {
+      const content = requestString.split(" ")[5].trim();
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
+      );
     } else {
-      socket.write(notFoundHTTPResponse);
-      console.log("404");
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
     socket.end();
   });
