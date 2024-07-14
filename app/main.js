b7121cdb3c247b9bed206fdbd2957877ba1b9fde
anthony <anthonycakins@gmail.com>
2024-07-14 16:21:33 +1000

    t

--- app/main.js
+++ app/main.js
@@ -35,6 +35,7 @@ const server = net.createServer((socket) => {
 
       if (method === "POST") {
         const reqBody = requestString.split("\r\n")[7];
+        console.log(reqBody)
 
         fs.appendFile(filePath + fileName, reqBody, (err) => {
           if (err) {
